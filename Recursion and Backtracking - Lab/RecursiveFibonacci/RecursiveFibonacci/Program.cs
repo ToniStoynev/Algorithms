9fae77ebaa40f2e4b5d9aa060d9759358fa7704e
GitHub99999 <toni.stoinev@gmail.com>
2020-11-22 21:35:50 +0200

    Added QueensPuzzle and Recursive Fibonacci

---
+++ Recursion and Backtracking - Lab/RecursiveFibonacci/RecursiveFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(RecursiveFibonacci(n));
+        }
+
+        private static int RecursiveFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+        }
+    }
+}
